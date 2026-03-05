22470dc946f6ba7f8e509bc6d4c3ea8cb5b6a2d8
Dom <dom@overpolish.co>
2026-03-05 19:34:43 +0000

    chore: update clangd template

--- KeyframelessKit/KeyframelessKit/Parameters/KKCustomGroupHeaderView/KKCustomGroupHeaderView.h
+++ KeyframelessKit/KeyframelessKit/Parameters/KKCustomGroupHeaderView/KKCustomGroupHeaderView.h
@@ -5,8 +5,8 @@
 //  Created by Dom on 27/02/2026.
 //
 
-#import <KeyframelessKit/KKNativeStyleView.h>
 #import <Cocoa/Cocoa.h>
+#import <KeyframelessKit/KKNativeStyleView.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
