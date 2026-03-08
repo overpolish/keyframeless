a1a49b52c7e44660042323a6a7f627710be42dff
Dom <dom@overpolish.co>
2026-03-08 19:28:17 +0000

    feat: update keyframe button icon is keyframe exists

--- KeyframelessKit/KeyframelessKit/Parameters/KKCustomGroupHeaderView/KKCustomGroupHeaderView.h
+++ KeyframelessKit/KeyframelessKit/Parameters/KKCustomGroupHeaderView/KKCustomGroupHeaderView.h
@@ -7,17 +7,13 @@
 
 #import <Cocoa/Cocoa.h>
 #import <KeyframelessKit/KKNativeStyleView.h>
+#include <MacTypes.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol PROAPIAccessing;
 
 @interface KKCustomGroupHeaderView : KKNativeStyleView
-
-- (instancetype)initWithFrame:(NSRect)frame
-                   apiManager:(id<PROAPIAccessing>)apiManager
-                        label:(NSString *)label;
-
 @end
 
 NS_ASSUME_NONNULL_END
