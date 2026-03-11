85cdb8c8721d8fad4503d174b10db6dd44da04be
Dom <dom@overpolish.co>
2026-03-11 09:38:34 +0000

    feat: parameter row optional left/right views

--- KeyframelessKit/KeyframelessKit/Parameters/KKParameterRowView/KKParameterRowView.h
+++ KeyframelessKit/KeyframelessKit/Parameters/KKParameterRowView/KKParameterRowView.h
@@ -14,6 +14,12 @@
 @property(nonatomic, strong) id<PROAPIAccessing> apiManager;
 @property(nonatomic, assign) UInt32 parameterId;
 
+/// Optional left section view. When nil, rightView fills all available space.
+@property(nonatomic, strong, nullable) NSView *leftView;
+
+/// Optional right section view. When nil, leftView fills all available space.
+@property(nonatomic, strong, nullable) NSView *rightView;
+
 - (instancetype)initWithFrame:(NSRect)frame
                    apiManager:(id<PROAPIAccessing>)apiManager
                   parameterId:(UInt32)parameterId;
