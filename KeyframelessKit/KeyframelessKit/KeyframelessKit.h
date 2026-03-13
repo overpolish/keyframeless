51a3a646d35b2575bf14abeb8d9be83cfbcdf536
Dom <dom@overpolish.co>
2026-03-13 14:38:28 +0000

    refactor: remove manual attempt to recreate full width control

--- KeyframelessKit/KeyframelessKit/KeyframelessKit.h
+++ KeyframelessKit/KeyframelessKit/KeyframelessKit.h
@@ -12,11 +12,8 @@ FOUNDATION_EXPORT const unsigned char
     KeyframelessKitVersionString[]; //! Version string
 
 #import <KeyframelessKit/KKArcOSC.h>
-#import <KeyframelessKit/KKCustomGroupHeaderView.h>
 #import <KeyframelessKit/KKMetalDeviceCache.h>
-#import <KeyframelessKit/KKNumberField.h>
 #import <KeyframelessKit/KKPlugin.h>
 #import <KeyframelessKit/KKPointOSC.h>
 #import <KeyframelessKit/KKRenderPrimitives.h>
 #import <KeyframelessKit/KKShaderTypes.h>
-#import <KeyframelessKit/KKSliderView.h>
--- KeyframelessKit/KeyframelessKit/NSColor+KKColors.m
+++ KeyframelessKit/KeyframelessKit/NSColor+KKColors.m
@@ -9,67 +9,59 @@
 @implementation NSColor (KKColors)
 
 + (NSColor *)inspectorLabel {
-  return [NSColor colorWithWhite:179.0 / 255.0 alpha:1.0];
+  return [NSColor colorWithRed:0xB3 / 255.0
+                         green:0xB3 / 255.0
+                          blue:0xB3 / 255.0
+                         alpha:1.0];
 }
 
 + (NSColor *)inspectorBackground {
   if ([KKHostInfo isRunningInFinalCut]) {
-    return [NSColor colorWithWhite:22.0 / 255.0 alpha:1.0];
+    return [NSColor colorWithRed:0x16 / 255.0
+                           green:0x16 / 255.0
+                            blue:0x16 / 255.0
+                           alpha:1.0];
   }
-  return [NSColor colorWithWhite:45.0 / 255.0 alpha:1.0];
+
+  return [NSColor colorWithRed:0x2D / 255.0
+                         green:0x2D / 255.0
+                          blue:0x2D / 255.0
+                         alpha:1.0];
 }
 
 + (NSColor *)primary {
-  return [NSColor colorWithRed:0.7f green:0.7f blue:0.7f alpha:0.65f];
+  return [NSColor colorWithRed:0xB3 / 255.0
+                         green:0xB3 / 255.0
+                          blue:0xB3 / 255.0
+                         alpha:0.65f];
 }
 
 + (NSColor *)outline {
-  return [NSColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.8f];
+  return [NSColor colorWithRed:0x00 / 255.0
+                         green:0x00 / 255.0
+                          blue:0x00 / 255.0
+                         alpha:0.8f];
 }
 
 + (NSColor *)hover {
-  return [NSColor colorWithRed:0.7f green:0.7f blue:0.7f alpha:0.8f];
+  return [NSColor colorWithRed:0xB3 / 255.0
+                         green:0xB3 / 255.0
+                          blue:0xB3 / 255.0
+                         alpha:0.8f];
 }
 
 + (NSColor *)active {
-  return [NSColor colorWithRed:0.7f green:0.7f blue:0.7f alpha:0.95f];
+  return [NSColor colorWithRed:0xB3 / 255.0
+                         green:0xB3 / 255.0
+                          blue:0xB3 / 255.0
+                         alpha:0.95f];
 }
 
 + (NSColor *)transparent {
-  return [NSColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.0f];
-}
-
-+ (NSColor *)sliderTrackBackground {
-  return [NSColor colorWithRed:0x17 / 255.0
-                         green:0x17 / 255.0
-                          blue:0x17 / 255.0
-                         alpha:1.0];
-}
-
-+ (NSColor *)sliderTrackFill {
-  return [NSColor colorWithRed:0x61 / 255.0
-                         green:0x68 / 255.0
-                          blue:0xF5 / 255.0
-                         alpha:1.0];
-}
-
-+ (NSColor *)sliderKnobFill {
-  return [NSColor colorWithWhite:0x80 / 255.0 alpha:1.0];
-}
-
-+ (NSColor *)sliderKnobOutline {
-  return [NSColor colorWithRed:0x17 / 255.0
-                         green:0x17 / 255.0
-                          blue:0x17 / 255.0
-                         alpha:1.0];
-}
-
-+ (NSColor *)expandChevron {
-  return [NSColor colorWithWhite:0x91 / 255.0 alpha:1.0];
-}
-
-+ (NSColor *)activeExpandChevron {
-  return [NSColor colorWithWhite:0x6e / 255.0 alpha:1.0];
+  return [NSColor colorWithRed:0x00 / 255.0
+                         green:0x00 / 255.0
+                          blue:0x00 / 255.0
+                         alpha:0.0f];
 }
 
 - (simd_float4)simdFloat4 {
