991b5c985b6aacdb8cced8b3ecbf46ed4b3fba4e
Dom <dom@overpolish.co>
2026-03-15 14:13:40 +0000

    feat: add info parameter view

--- KeyframelessKit/KeyframelessKit/Parameter/KKInfoParameterView.h
+++ KeyframelessKit/KeyframelessKit/Parameter/KKInfoParameterView.h
@@ -19,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (nullable instancetype)initWithCoder:(NSCoder *)coder NS_UNAVAILABLE;
 
 @property(nonatomic, copy) NSString *text;
+@property(nonatomic, strong, readonly) NSView *container;
 
 @end
 
--- KeyframelessKit/KeyframelessKit/Parameter/KKInfoParameterView.m
+++ KeyframelessKit/KeyframelessKit/Parameter/KKInfoParameterView.m
@@ -4,43 +4,90 @@
  */
 
 #import "KKInfoParameterView.h"
+#import "KKHostInfo.h"
+#import "NSColor+KKColors.h"
+#import <AppKit/AppKit.h>
+#include <AppKit/NSView.h>
+#import <CoreFoundation/CFCGTypes.h>
+#import <Foundation/Foundation.h>
+#import <KeyframelessKit/KKLog.h>
 
 static const CGFloat KKInfoParameterViewVerticalPadding = 6.0;
+static const CGFloat KKInfoParameterViewHorizontalPadding = 21.0;
 
 @implementation KKInfoParameterView {
   NSTextField *_label;
+  KKLog *_log;
+  NSView *_container;
 }
 
+// Note: parameter view height is determined by the child view, not the parent.
+// Once set it cannot be changed, the space is preallocated.
+//
+// Width is also controlled by the child view, the parent resizes to match the
+// overall inspector.
+
 - (instancetype)initWithText:(NSString *)text {
-  self = [super initWithFrame:NSMakeRect(0, 0, 300, 28)];
+  self = [super initWithFrame:NSMakeRect(0, 0, 0.0, 46.0)]; // height of 2 rows
+  _log = [KKLog loggerForPlugin:@"co.overpolish.keyframeless"];
+
   if (self) {
     _text = [text copy];
-
-    _label = [NSTextField labelWithString:text];
-    _label.translatesAutoresizingMaskIntoConstraints = NO;
-    _label.textColor = NSColor.secondaryLabelColor;
-    _label.font = [NSFont systemFontOfSize:NSFont.smallSystemFontSize];
+    _label = [[NSTextField alloc] initWithFrame:self.bounds];
+    _label.stringValue = _text;
+    _label.textColor = [NSColor warning];
+    _label.backgroundColor = [NSColor clearColor];
+    _label.font = [KKInfoParameterView labelFont];
     _label.lineBreakMode = NSLineBreakByWordWrapping;
-    _label.maximumNumberOfLines = 0;
+    _label.maximumNumberOfLines = 2;
+    _label.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    _label.editable = NO;
+    _label.selectable = NO;
+    _label.bordered = NO;
+
     [self addSubview:_label];
 
+    self.translatesAutoresizingMaskIntoConstraints = NO;
+    _container = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, 0.0, 46.0)];
+    _container.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    [_container addSubview:self];
+
     [NSLayoutConstraint activateConstraints:@[
-      [_label.leadingAnchor constraintEqualToAnchor:self.leadingAnchor],
-      [_label.trailingAnchor constraintEqualToAnchor:self.trailingAnchor],
-      [_label.topAnchor
-          constraintEqualToAnchor:self.topAnchor
+      [self.topAnchor
+          constraintEqualToAnchor:_container.topAnchor
                          constant:KKInfoParameterViewVerticalPadding],
-      [_label.bottomAnchor
-          constraintEqualToAnchor:self.bottomAnchor
+      [self.leadingAnchor
+          constraintEqualToAnchor:_container.leadingAnchor
+                         constant:KKInfoParameterViewHorizontalPadding],
+      [self.bottomAnchor
+          constraintEqualToAnchor:_container.bottomAnchor
                          constant:-KKInfoParameterViewVerticalPadding],
+      [self.trailingAnchor
+          constraintEqualToAnchor:_container.trailingAnchor
+                         constant:-KKInfoParameterViewHorizontalPadding],
     ]];
   }
   return self;
 }
 
+- (NSView *)container {
+  return _container;
+}
+
++ (NSFont *)labelFont {
+  static NSFont *font = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    font = [NSFont systemFontOfSize:[NSFont smallSystemFontSize]
+                             weight:NSFontWeightLight];
+  });
+  return font;
+}
+
 - (void)setText:(NSString *)text {
   _text = [text copy];
   _label.stringValue = text;
+  [self setFrameSize:NSMakeSize(self.frame.size.width, 0)];
 }
 
 @end
