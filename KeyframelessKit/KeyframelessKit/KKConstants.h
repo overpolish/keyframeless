03b599b2f104d49e38a22f75ad22f460a95f64b4
Dom <dom@overpolish.co>
2026-03-16 18:05:24 +0000

    feat: render parameter views in swift

--- KeyframelessKit/KeyframelessKit/KKConstants.h
+++ KeyframelessKit/KeyframelessKit/KKConstants.h
@@ -13,8 +13,8 @@
 /// 9000–9899  decorative UI parameters (info labels, separators)
 /// 9900–9999  animation system parameters (added by KKPlugin)
 
-static const UInt32 kKKParamAnimationSeparator = 9900;
-static const UInt32 kKKParamAnimateIn = 9901;
-static const UInt32 kKKParamAnimateOut = 9902;
-static const UInt32 kKKParamAnimationDuration = 9903;
-static const UInt32 kKKParamAnimationInterpolation = 9904;
+static const UInt32 kKKParamAnimationSeparator __attribute__((unused)) = 9900;
+static const UInt32 kKKParamAnimateIn __attribute__((unused)) = 9901;
+static const UInt32 kKKParamAnimateOut __attribute__((unused)) = 9902;
+static const UInt32 kKKParamAnimationDuration __attribute__((unused)) = 9903;
+static const UInt32 kKKParamAnimationInterpolation __attribute__((unused)) = 9904;
