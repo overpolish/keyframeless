569ded729788d9d0950a05802cde326826d65986
Dom <dom@overpolish.co>
2026-03-17 09:46:30 +0000

    refactor: move fcpxmlparser into core

--- Keyframeless X/Keyframeless X FCP/Core/FCPXMLParser.swift
+++ Keyframeless X/Keyframeless X FCP/Core/FCPXMLParser.swift
@@ -3,60 +3,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import AppKit
-import SwiftUI
-
-struct FCPDropZoneView: NSViewRepresentable {
-	var onDrop: ([FCPXMLParser.AudioClip]) -> Void
-
-	func makeNSView(context: Context) -> FCPDropTargetView {
-		let view = FCPDropTargetView()
-		view.onDrop = onDrop
-		return view
-	}
-
-	func updateNSView(_ nsView: FCPDropTargetView, context: Context) {
-		nsView.onDrop = onDrop
-	}
-}
-
-class FCPDropTargetView: NSView {
-	var onDrop: (([FCPXMLParser.AudioClip]) -> Void)?
-
-	private let fcpPasteboardTypes: [NSPasteboard.PasteboardType] = [
-		"com.apple.finalcutpro.xml.v1-10",
-		"com.apple.finalcutpro.xml.v1-9",
-		"com.apple.finalcutpro.xml",
-	].map { NSPasteboard.PasteboardType($0) }
-
-	override init(frame: NSRect) {
-		super.init(frame: frame)
-		registerForDraggedTypes(fcpPasteboardTypes)
-	}
-
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) not implemented")
-	}
-
-	override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
-		let available = sender.draggingPasteboard.types ?? []
-		return fcpPasteboardTypes.contains(where: { available.contains($0) }) ? .copy : []
-	}
-
-	override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
-		let pasteboard = sender.draggingPasteboard
-		for type in fcpPasteboardTypes {
-			guard let available = pasteboard.types, available.contains(type),
-				let data = pasteboard.data(forType: type)
-			else { continue }
-			if let doc = try? XMLDocument(data: data, options: []) {
-				onDrop?(FCPXMLParser.audioClips(in: doc))
-			}
-			return true
-		}
-		return false
-	}
-}
+import Foundation
 
 enum FCPXMLParser {
 
@@ -68,6 +15,24 @@ enum FCPXMLParser {
 		let sourceDuration: Double
 		let url: URL?
 		let bookmark: Data?
+
+		func data() throws -> Data {
+			if let bookmark {
+				var isStale = false
+				if let scopedURL = try? URL(
+					resolvingBookmarkData: bookmark,
+					options: .withSecurityScope,
+					relativeTo: nil,
+					bookmarkDataIsStale: &isStale
+				) {
+					let accessing = scopedURL.startAccessingSecurityScopedResource()
+					defer { if accessing { scopedURL.stopAccessingSecurityScopedResource() } }
+					return try Data(contentsOf: scopedURL)
+				}
+			}
+			guard let url else { throw CocoaError(.fileNoSuchFile) }
+			return try Data(contentsOf: url)
+		}
 	}
 
 	static func audioClips(in doc: XMLDocument) -> [AudioClip] {
--- Keyframeless X/Keyframeless X FCP/Features/Captions/AudioPlayer.swift
+++ Keyframeless X/Keyframeless X FCP/Features/Captions/AudioPlayer.swift
@@ -21,7 +21,7 @@ final class AudioPlayer: ObservableObject {
 			return
 		}
 		stop()
-		guard let data = try? resolveData(for: clip),
+		guard let data = try? clip.data(),
 			let newPlayer = try? AVAudioPlayer(data: data)
 		else { return }
 		newPlayer.prepareToPlay()
@@ -38,23 +38,5 @@ final class AudioPlayer: ObservableObject {
 		player?.stop()
 		player = nil
 		playingURL = nil
-	}
-
-	private func resolveData(for clip: FCPXMLParser.AudioClip) throws -> Data {
-		if let bookmark = clip.bookmark {
-			var isStale = false
-			if let scopedURL = try? URL(
-				resolvingBookmarkData: bookmark,
-				options: .withSecurityScope,
-				relativeTo: nil,
-				bookmarkDataIsStale: &isStale
-			) {
-				let accessing = scopedURL.startAccessingSecurityScopedResource()
-				defer { if accessing { scopedURL.stopAccessingSecurityScopedResource() } }
-				return try Data(contentsOf: scopedURL)
-			}
-		}
-		guard let url = clip.url else { throw CocoaError(.fileNoSuchFile) }
-		return try Data(contentsOf: url)
 	}
 }
