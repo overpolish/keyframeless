83db6998737c684eb2efe9d7ceb43b1c90bb135d
Dom <dom@overpolish.co>
2026-03-19 19:03:14 +0000

    refactor: pull out views out from captionsview

---
+++ Keyframeless X/Keyframeless X FCP/Features/Captions/CaptionItemRow.swift
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2026 overpolish
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import KeyframelessKit
+import SwiftUI
+
+struct CaptionItemRow: View {
+	let name: String
+	let isHidden: Bool
+	let clips: [FCPXMLParser.AudioClip]
+	@Binding var selectedClips: Set<Int>
+
+	var body: some View {
+		HStack {
+			Text(name)
+				.font(.title2)
+				.lineLimit(1)
+				.opacity(isHidden ? 0 : 1)
+			Spacer()
+			ClipSelectionToolbar(clips: clips, selectedClips: $selectedClips)
+		}
+		.padding(.horizontal, KKPaddingLG)
+	}
+}
