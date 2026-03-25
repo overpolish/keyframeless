7a654beead54f9e2c98cdcdd0364d211553f5eb0
Dom <dom@overpolish.co>
2026-03-25 08:35:44 +0000

    chore: boilerplate for MagicMove plugin

---
+++ MagicMove/MagicMove/Plugin/Constants.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2026 overpolish
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+static NSString *const kPluginID = @"co.overpolish.keyframeless.MagicMove";
+
+// Parameter IDs
+static const UInt32 kParamRadius = 1;
