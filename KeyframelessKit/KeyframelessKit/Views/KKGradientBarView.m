5f5b63d4b9ed39a30773becddf4c13b761ac925c
Dom <dom@overpolish.co>
2026-04-08 14:03:41 +0100

    feat(KeyframelessKit): gradient midpoint support

--- KeyframelessKit/KeyframelessKit/Views/KKGradientBarView.m
+++ KeyframelessKit/KeyframelessKit/Views/KKGradientBarView.m
@@ -12,18 +12,36 @@ static const CGFloat kStopDiameter = 10.0;
 static const CGFloat kStopHitRadius = 8.0;
 static const CGFloat kDeleteDragDistance = 30.0;
 static const NSInteger kMinStops = 2;
+static const CGFloat kMidpointSize = 7.0;
+static const CGFloat kMidpointHitRadius = 6.0;
 
 @implementation KKGradientStop
 
 + (instancetype)stopWithPosition:(CGFloat)position color:(NSColor *)color {
+  return [self stopWithPosition:position color:color midpoint:0.5];
+}
+
++ (instancetype)stopWithPosition:(CGFloat)position
+                           color:(NSColor *)color
+                        midpoint:(CGFloat)midpoint {
   KKGradientStop *stop = [[KKGradientStop alloc] init];
   stop.position = position;
   stop.color = color;
+  stop.midpoint = midpoint;
   return stop;
 }
 
+- (instancetype)init {
+  self = [super init];
+  if (self)
+    _midpoint = 0.5;
+  return self;
+}
+
 - (id)copyWithZone:(NSZone *)zone {
-  return [KKGradientStop stopWithPosition:_position color:[_color copy]];
+  return [KKGradientStop stopWithPosition:_position
+                                    color:[_color copy]
+                                 midpoint:_midpoint];
 }
 
 @end
@@ -34,8 +52,7 @@ void KKDrawCheckerboard(NSRect rect) {
   CGFloat checkSize = 4.0;
   for (CGFloat y = rect.origin.y; y < NSMaxY(rect); y += checkSize) {
     for (CGFloat x = rect.origin.x; x < NSMaxX(rect); x += checkSize) {
-      BOOL isLight =
-          ((int)(x / checkSize) + (int)(y / checkSize)) % 2 == 0;
+      BOOL isLight = ((int)(x / checkSize) + (int)(y / checkSize)) % 2 == 0;
       [isLight ? light : dark setFill];
       NSRectFill(NSMakeRect(x, y, checkSize, checkSize));
     }
@@ -44,6 +61,7 @@ void KKDrawCheckerboard(NSRect rect) {
 
 @implementation KKGradientBarView {
   NSInteger _dragIndex;
+  NSInteger _dragMidpointIndex; // sorted index of midpoint being dragged
   BOOL _dragStarted;
   NSPoint _dragOrigin;
   NSInteger _colorPickerStopIndex;
@@ -58,7 +76,9 @@ void KKDrawCheckerboard(NSRect rect) {
       [KKGradientStop stopWithPosition:1.0 color:[NSColor whiteColor]]
     ];
     _selectedIndex = 0;
+    _selectedMidpointIndex = -1;
     _dragIndex = -1;
+    _dragMidpointIndex = -1;
     _interactionEnabled = YES;
   }
   return self;
@@ -96,10 +116,9 @@ void KKDrawCheckerboard(NSRect rect) {
 
 - (void)drawRect:(NSRect)dirtyRect {
   NSRect bar = [self _barRect];
-  NSBezierPath *barClip =
-      [NSBezierPath bezierPathWithRoundedRect:bar
-                                      xRadius:KKRadiusSM
-                                      yRadius:KKRadiusSM];
+  NSBezierPath *barClip = [NSBezierPath bezierPathWithRoundedRect:bar
+                                                          xRadius:KKRadiusSM
+                                                          yRadius:KKRadiusSM];
 
   // Checkerboard background for transparency
   [NSGraphicsContext saveGraphicsState];
@@ -108,29 +127,45 @@ void KKDrawCheckerboard(NSRect rect) {
   [NSGraphicsContext restoreGraphicsState];
 
   // Gradient fill
-  NSMutableArray<KKGradientStop *> *sorted =
-      [[_stops sortedArrayUsingComparator:^(KKGradientStop *a,
-                                            KKGradientStop *b) {
-        if (a.position < b.position) return NSOrderedAscending;
-        if (a.position > b.position) return NSOrderedDescending;
+  NSMutableArray<KKGradientStop *> *sorted = [[_stops
+      sortedArrayUsingComparator:^(KKGradientStop *a, KKGradientStop *b) {
+        if (a.position < b.position)
+          return NSOrderedAscending;
+        if (a.position > b.position)
+          return NSOrderedDescending;
         return NSOrderedSame;
       }] mutableCopy];
 
   if (sorted.count >= 2) {
-    CGFloat *locations = malloc(sizeof(CGFloat) * sorted.count);
-    for (NSUInteger i = 0; i < sorted.count; i++)
-      locations[i] = sorted[i].position;
+    NSMutableArray<NSColor *> *colors = [NSMutableArray new];
+    NSMutableArray<NSNumber *> *locs = [NSMutableArray new];
+    for (NSUInteger i = 0; i < sorted.count; i++) {
+      [colors addObject:sorted[i].color];
+      [locs addObject:@(sorted[i].position)];
+      if (i < sorted.count - 1) {
+        CGFloat m = sorted[i].midpoint;
+        CGFloat midPos = sorted[i].position +
+                         m * (sorted[i + 1].position - sorted[i].position);
+        NSColor *midColor =
+            [sorted[i].color blendedColorWithFraction:0.5
+                                              ofColor:sorted[i + 1].color];
+        [colors addObject:midColor];
+        [locs addObject:@(midPos)];
+      }
+    }
+    CGFloat *locations = malloc(sizeof(CGFloat) * locs.count);
+    for (NSUInteger i = 0; i < locs.count; i++)
+      locations[i] = locs[i].doubleValue;
 
-    NSGradient *gradient = [[NSGradient alloc]
-        initWithColors:[sorted valueForKey:@"color"]
-           atLocations:locations
-            colorSpace:[NSColorSpace sRGBColorSpace]];
+    NSGradient *gradient =
+        [[NSGradient alloc] initWithColors:colors
+                               atLocations:locations
+                                colorSpace:[NSColorSpace sRGBColorSpace]];
     free(locations);
 
-    NSBezierPath *barPath =
-        [NSBezierPath bezierPathWithRoundedRect:bar
-                                        xRadius:KKRadiusSM
-                                        yRadius:KKRadiusSM];
+    NSBezierPath *barPath = [NSBezierPath bezierPathWithRoundedRect:bar
+                                                            xRadius:KKRadiusSM
+                                                            yRadius:KKRadiusSM];
     [gradient drawInBezierPath:barPath angle:0];
   }
 
@@ -150,8 +185,7 @@ void KKDrawCheckerboard(NSRect rect) {
     CGFloat y = NSMaxY(bar) + KKSpacingXS;
 
     NSRect handleRect =
-        NSMakeRect(x - kStopDiameter * 0.5, y,
-                   kStopDiameter, kStopDiameter);
+        NSMakeRect(x - kStopDiameter * 0.5, y, kStopDiameter, kStopDiameter);
 
     // Color swatch
     [stop.color setFill];
@@ -160,15 +194,44 @@ void KKDrawCheckerboard(NSRect rect) {
     [handle fill];
 
     // Handle border
-    NSColor *borderColor = (i == _selectedIndex)
-                               ? [NSColor accentMatchingHost]
-                               : [NSColor.inspectorLabel
-                                     colorWithAlphaComponent:0.4];
+    NSColor *borderColor =
+        (i == _selectedIndex)
+            ? [NSColor accentMatchingHost]
+            : [NSColor.inspectorLabel colorWithAlphaComponent:0.4];
     [borderColor setStroke];
-    NSBezierPath *ring =
-        [NSBezierPath bezierPathWithOvalInRect:NSInsetRect(handleRect, 0.5, 0.5)];
+    NSBezierPath *ring = [NSBezierPath
+        bezierPathWithOvalInRect:NSInsetRect(handleRect, 0.5, 0.5)];
     ring.lineWidth = (i == _selectedIndex) ? KKBorderWidthSM : KKBorderWidthXS;
     [ring stroke];
+  }
+
+  // Midpoint diamonds
+  for (NSUInteger i = 0; i < sorted.count - 1; i++) {
+    CGFloat m = sorted[i].midpoint;
+    CGFloat midPos =
+        sorted[i].position + m * (sorted[i + 1].position - sorted[i].position);
+    CGFloat mx = [self _xForPosition:midPos];
+    CGFloat my = NSMaxY(bar) + KKSpacingXS + kStopDiameter * 0.5;
+    CGFloat half = kMidpointSize * 0.5;
+
+    NSBezierPath *diamond = [NSBezierPath bezierPath];
+    [diamond moveToPoint:NSMakePoint(mx, my - half)];
+    [diamond lineToPoint:NSMakePoint(mx + half, my)];
+    [diamond lineToPoint:NSMakePoint(mx, my + half)];
+    [diamond lineToPoint:NSMakePoint(mx - half, my)];
+    [diamond closePath];
+
+    BOOL selected = ((NSInteger)i == _selectedMidpointIndex);
+    [[NSColor.inspectorLabel colorWithAlphaComponent:selected ? 0.6 : 0.3]
+        setFill];
+    [diamond fill];
+
+    NSColor *dmBorder =
+        selected ? [NSColor accentMatchingHost]
+                 : [NSColor.inspectorLabel colorWithAlphaComponent:0.4];
+    [dmBorder setStroke];
+    diamond.lineWidth = selected ? KKBorderWidthSM : KKBorderWidthXS;
+    [diamond stroke];
   }
 }
 
@@ -184,9 +247,37 @@ void KKDrawCheckerboard(NSRect rect) {
   return -1;
 }
 
+- (NSArray<KKGradientStop *> *)_sortedStops {
+  return [_stops
+      sortedArrayUsingComparator:^(KKGradientStop *a, KKGradientStop *b) {
+        if (a.position < b.position)
+          return NSOrderedAscending;
+        if (a.position > b.position)
+          return NSOrderedDescending;
+        return NSOrderedSame;
+      }];
+}
+
+- (NSInteger)_midpointIndexAtPoint:(NSPoint)point {
+  NSRect bar = [self _barRect];
+  NSArray<KKGradientStop *> *sorted = [self _sortedStops];
+  CGFloat cy = NSMaxY(bar) + KKSpacingXS + kStopDiameter * 0.5;
+  for (NSUInteger i = 0; i < sorted.count - 1; i++) {
+    CGFloat m = sorted[i].midpoint;
+    CGFloat midPos =
+        sorted[i].position + m * (sorted[i + 1].position - sorted[i].position);
+    CGFloat mx = [self _xForPosition:midPos];
+    CGFloat dist = hypot(point.x - mx, point.y - cy);
+    if (dist <= kMidpointHitRadius)
+      return (NSInteger)i;
+  }
+  return -1;
+}
+
 - (void)resetCursorRects {
   if (!_interactionEnabled) {
-    [self addCursorRect:self.bounds cursor:[NSCursor operationNotAllowedCursor]];
+    [self addCursorRect:self.bounds
+                 cursor:[NSCursor operationNotAllowedCursor]];
   }
 }
 
@@ -209,20 +300,44 @@ void KKDrawCheckerboard(NSRect rect) {
 
   if (hitIndex >= 0) {
     _dragIndex = hitIndex;
+    _dragMidpointIndex = -1;
     _dragStarted = NO;
     _dragOrigin = point;
+    _selectedMidpointIndex = -1;
     [self _selectIndex:hitIndex];
-  } else {
-    NSRect bar = [self _barRect];
-    if (NSPointInRect(point, NSInsetRect(bar, 0, -kStopDiameter))) {
-      CGFloat pos = [self _positionForX:point.x];
-      [self _addStopAtPosition:pos];
-    }
+    return;
+  }
+
+  NSInteger midHit = [self _midpointIndexAtPoint:point];
+  if (event.clickCount == 2 && midHit >= 0) {
+    NSArray<KKGradientStop *> *sorted = [self _sortedStops];
+    KKGradientStop *owner = sorted[midHit];
+    NSInteger origIdx = [_stops indexOfObjectIdenticalTo:owner];
+    if (origIdx != NSNotFound)
+      [self setMidpoint:0.5 forStopAtIndex:origIdx];
+    return;
+  }
+  if (midHit >= 0) {
+    _dragMidpointIndex = midHit;
+    _dragIndex = -1;
+    _dragStarted = NO;
+    _dragOrigin = point;
+    _selectedMidpointIndex = midHit;
+    [self _selectIndex:-1];
+    [self setNeedsDisplay:YES];
+    return;
+  }
+
+  NSRect bar = [self _barRect];
+  if (NSPointInRect(point, NSInsetRect(bar, 0, -kStopDiameter))) {
+    _selectedMidpointIndex = -1;
+    CGFloat pos = [self _positionForX:point.x];
+    [self _addStopAtPosition:pos];
   }
 }
 
 - (void)mouseDragged:(NSEvent *)event {
-  if (_dragIndex < 0)
+  if (_dragIndex < 0 && _dragMidpointIndex < 0)
     return;
 
   NSPoint point = [self convertPoint:event.locationInWindow fromView:nil];
@@ -231,6 +346,33 @@ void KKDrawCheckerboard(NSRect rect) {
     if (hypot(point.x - _dragOrigin.x, point.y - _dragOrigin.y) < 3.0)
       return;
     _dragStarted = YES;
+  }
+
+  if (_dragMidpointIndex >= 0) {
+    NSArray<KKGradientStop *> *sorted = [self _sortedStops];
+    NSUInteger idx = (NSUInteger)_dragMidpointIndex;
+    if (idx >= sorted.count - 1)
+      return;
+    KKGradientStop *lo = sorted[idx];
+    KKGradientStop *hi = sorted[idx + 1];
+    CGFloat segLen = hi.position - lo.position;
+    if (segLen <= 0)
+      return;
+    CGFloat pos = [self _positionForX:point.x];
+    CGFloat m = (pos - lo.position) / segLen;
+    m = fmax(0.05, fmin(0.95, m));
+    NSInteger origIdx = [_stops indexOfObjectIdenticalTo:lo];
+    if (origIdx == NSNotFound)
+      return;
+    NSMutableArray *mutable = [_stops mutableCopy];
+    mutable[origIdx] = [KKGradientStop stopWithPosition:lo.position
+                                                  color:lo.color
+                                               midpoint:m];
+    _stops = [mutable copy];
+    [self setNeedsDisplay:YES];
+    if (_onStopsChanged)
+      _onStopsChanged(_stops);
+    return;
   }
 
   NSRect bar = [self _barRect];
@@ -247,7 +389,8 @@ void KKDrawCheckerboard(NSRect rect) {
   NSMutableArray *mutable = [_stops mutableCopy];
   mutable[_dragIndex] =
       [KKGradientStop stopWithPosition:pos
-                                 color:_stops[_dragIndex].color];
+                                 color:_stops[_dragIndex].color
+                              midpoint:_stops[_dragIndex].midpoint];
   _stops = [mutable copy];
   [self setNeedsDisplay:YES];
   if (_onStopsChanged)
@@ -256,6 +399,7 @@ void KKDrawCheckerboard(NSRect rect) {
 
 - (void)mouseUp:(NSEvent *)event {
   _dragIndex = -1;
+  _dragMidpointIndex = -1;
   _dragStarted = NO;
 }
 
@@ -303,8 +447,8 @@ void KKDrawCheckerboard(NSRect rect) {
 
 - (void)_addStopAtPosition:(CGFloat)position {
   NSColor *color = [self _interpolatedColorAtPosition:position];
-  KKGradientStop *newStop =
-      [KKGradientStop stopWithPosition:position color:color];
+  KKGradientStop *newStop = [KKGradientStop stopWithPosition:position
+                                                       color:color];
 
   NSMutableArray *mutable = [_stops mutableCopy];
   [mutable addObject:newStop];
@@ -340,11 +484,12 @@ void KKDrawCheckerboard(NSRect rect) {
 }
 
 - (NSColor *)_interpolatedColorAtPosition:(CGFloat)position {
-  NSArray<KKGradientStop *> *sorted =
-      [_stops sortedArrayUsingComparator:^(KKGradientStop *a,
-                                           KKGradientStop *b) {
-        if (a.position < b.position) return NSOrderedAscending;
-        if (a.position > b.position) return NSOrderedDescending;
+  NSArray<KKGradientStop *> *sorted = [_stops
+      sortedArrayUsingComparator:^(KKGradientStop *a, KKGradientStop *b) {
+        if (a.position < b.position)
+          return NSOrderedAscending;
+        if (a.position > b.position)
+          return NSOrderedDescending;
         return NSOrderedSame;
       }];
 
@@ -360,6 +505,13 @@ void KKDrawCheckerboard(NSRect rect) {
     KKGradientStop *b = sorted[i + 1];
     if (position >= a.position && position <= b.position) {
       CGFloat t = (position - a.position) / (b.position - a.position);
+      CGFloat m = a.midpoint;
+      if (m > 0.0 && m < 1.0) {
+        if (t <= m)
+          t = 0.5 * (t / m);
+        else
+          t = 0.5 + 0.5 * ((t - m) / (1.0 - m));
+      }
       return [a.color blendedColorWithFraction:t ofColor:b.color];
     }
   }
@@ -370,8 +522,9 @@ void KKDrawCheckerboard(NSRect rect) {
   if (index < 0 || index >= (NSInteger)_stops.count)
     return;
   NSMutableArray *mutable = [_stops mutableCopy];
-  mutable[index] =
-      [KKGradientStop stopWithPosition:_stops[index].position color:color];
+  mutable[index] = [KKGradientStop stopWithPosition:_stops[index].position
+                                              color:color
+                                           midpoint:_stops[index].midpoint];
   _stops = [mutable copy];
   [self setNeedsDisplay:YES];
   if (_onStopsChanged)
@@ -383,8 +536,23 @@ void KKDrawCheckerboard(NSRect rect) {
     return;
   position = fmax(0.0, fmin(1.0, position));
   NSMutableArray *mutable = [_stops mutableCopy];
-  mutable[index] =
-      [KKGradientStop stopWithPosition:position color:_stops[index].color];
+  mutable[index] = [KKGradientStop stopWithPosition:position
+                                              color:_stops[index].color
+                                           midpoint:_stops[index].midpoint];
+  _stops = [mutable copy];
+  [self setNeedsDisplay:YES];
+  if (_onStopsChanged)
+    _onStopsChanged(_stops);
+}
+
+- (void)setMidpoint:(CGFloat)midpoint forStopAtIndex:(NSInteger)index {
+  if (index < 0 || index >= (NSInteger)_stops.count)
+    return;
+  midpoint = fmax(0.05, fmin(0.95, midpoint));
+  NSMutableArray *mutable = [_stops mutableCopy];
+  mutable[index] = [KKGradientStop stopWithPosition:_stops[index].position
+                                              color:_stops[index].color
+                                           midpoint:midpoint];
   _stops = [mutable copy];
   [self setNeedsDisplay:YES];
   if (_onStopsChanged)
