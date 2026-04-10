63d223aaad27e2bf98e15dc4e2a5fa09bf18689d
Dom <dom@overpolish.co>
2026-04-10 09:52:12 +0100

    feat(Rounded): per property animate in/out

--- Rounded/Rounded/Plugin/Constants.h
+++ Rounded/Rounded/Plugin/Constants.h
@@ -9,10 +9,8 @@
 
 static NSString *const kPluginID = @"co.overpolish.keyframeless.Rounded";
 
-// Parameter IDs
 static const UInt32 kParamRadius = 1;
 
-// Crop group
 static const UInt32 kParamCropGroup = 10;
 static const UInt32 kParamCropExpanded = 11;
 static const UInt32 kParamCropTop = 12;
@@ -20,5 +18,11 @@ static const UInt32 kParamCropBottom = 13;
 static const UInt32 kParamCropLeft = 14;
 static const UInt32 kParamCropRight = 15;
 
-// OSC part IDs
+static const UInt32 kParamInRadius = 600;
+static const UInt32 kParamInCrop = 601;
+static const UInt32 kParamHoldRadius = 500;
+static const UInt32 kParamHoldCrop = 501;
+static const UInt32 kParamOutRadius = 700;
+static const UInt32 kParamOutCrop = 701;
+
 static const NSInteger kOSCRadiusPart = 100;
