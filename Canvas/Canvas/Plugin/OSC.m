6253f7178b43b50b50728ea8722864d827647ccb
Dom <dom@overpolish.co>
2026-04-14 09:26:34 +0100

    feat(Canvas): layer grouping

--- Canvas/Canvas/Plugin/OSC.m
+++ Canvas/Canvas/Plugin/OSC.m
@@ -91,7 +91,8 @@ NSUInteger selKey(NSUInteger pathIdx, NSUInteger ptIdx) {
 - (KKBezierPath *)activePath {
   if (self.activePathIndex >= 0 &&
       self.activePathIndex < (NSInteger)self.paths.count &&
-      !self.paths[self.activePathIndex].locked)
+      !self.paths[self.activePathIndex].locked &&
+      !self.paths[self.activePathIndex].isGroup)
     return self.paths[self.activePathIndex];
   return nil;
 }
@@ -130,7 +131,7 @@ NSUInteger selKey(NSUInteger pathIdx, NSUInteger ptIdx) {
 - (NSInteger)pathIndexNearX:(double)x y:(double)y radius:(double)radius {
   for (NSUInteger p = 0; p < self.paths.count; p++) {
     KKBezierPath *path = self.paths[p];
-    if (path.hidden || path.locked)
+    if (path.hidden || path.locked || path.isGroup)
       continue;
     NSUInteger segCount = path.count - 1;
     if (path.closed && path.count >= 2)
