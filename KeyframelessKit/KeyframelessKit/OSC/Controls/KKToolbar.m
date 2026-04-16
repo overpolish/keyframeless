8b98e60db6e554bdd34255887f5dd86839686e59
Dom <dom@overpolish.co>
2026-04-16 21:45:41 +0100

    feat(Canvas): toolbar shortcuts
    Only work when mouse clicked into canvas OSC

--- KeyframelessKit/KeyframelessKit/OSC/Controls/KKToolbar.m
+++ KeyframelessKit/KeyframelessKit/OSC/Controls/KKToolbar.m
@@ -15,6 +15,7 @@ static NSString *const kToolbarPipelineID =
 
 static const CGFloat kIconSize = 28.0;
 static const CGFloat kButtonSize = 48.0;
+static const CGFloat kButtonHeight = 64.0;
 static const CGFloat kButtonSpacing = 8.0;
 static const CGFloat kToolbarPadding = 14.0;
 static const CGFloat kToolbarMargin = 8.0;
@@ -24,20 +25,29 @@ static const CGFloat kHighlightCorner = 8.0;
 
 @implementation KKToolbarItem
 
-+ (instancetype)itemWithIcon:(NSString *)sfSymbolName tag:(NSInteger)tag {
++ (instancetype)itemWithIcon:(NSString *)sfSymbolName
+                         tag:(NSInteger)tag
+               shortcutLabel:(NSString *)shortcutLabel {
   KKToolbarItem *item = [[KKToolbarItem alloc] init];
   item.iconName = sfSymbolName;
   item.tag = tag;
+  item.shortcutLabel = shortcutLabel;
   return item;
 }
 
 @end
+
+static const CGFloat kLabelFontSize = 12.0;
+static const CGFloat kLabelHeight = 16.0;
+static const CGFloat kIconShiftY = 8.0;
 
 @implementation KKToolbar {
   id<PROAPIAccessing> __weak _apiManager;
   NSArray<KKToolbarItem *> *_items;
   NSMutableArray *_iconTextures;
   NSMutableArray<NSString *> *_cachedNames;
+  NSMutableArray *_labelTextures;
+  NSMutableArray<NSString *> *_cachedLabels;
   NSMutableArray<NSValue *> *_buttonCenters;
   NSMutableArray<NSValue *> *_buttonRects;
   id<MTLTexture> _bgTexture;
@@ -56,11 +66,15 @@ static const CGFloat kHighlightCorner = 8.0;
     _activeTag = items.firstObject.tag;
     _iconTextures = [NSMutableArray arrayWithCapacity:items.count];
     _cachedNames = [NSMutableArray arrayWithCapacity:items.count];
+    _labelTextures = [NSMutableArray arrayWithCapacity:items.count];
+    _cachedLabels = [NSMutableArray arrayWithCapacity:items.count];
     _buttonCenters = [NSMutableArray arrayWithCapacity:items.count];
     _buttonRects = [NSMutableArray arrayWithCapacity:items.count];
     for (NSUInteger i = 0; i < items.count; i++) {
       [_iconTextures addObject:[NSNull null]];
       [_cachedNames addObject:@""];
+      [_labelTextures addObject:[NSNull null]];
+      [_cachedLabels addObject:@""];
       [_buttonCenters addObject:[NSValue valueWithPoint:NSZeroPoint]];
       [_buttonRects addObject:[NSValue valueWithRect:NSZeroRect]];
     }
@@ -186,6 +200,65 @@ static id<MTLTexture> renderRoundedRect(id<MTLDevice> device, CGFloat w,
   return texture;
 }
 
+- (id<MTLTexture>)textureForLabel:(NSString *)label
+                           device:(id<MTLDevice>)device
+                            index:(NSUInteger)idx {
+  if (!label.length)
+    return nil;
+  if (idx < _labelTextures.count && _labelTextures[idx] != (id)[NSNull null] &&
+      [_cachedLabels[idx] isEqualToString:label])
+    return _labelTextures[idx];
+
+  NSDictionary *attrs = @{
+    NSFontAttributeName : [NSFont systemFontOfSize:kLabelFontSize
+                                            weight:NSFontWeightMedium],
+    NSForegroundColorAttributeName : [NSColor colorWithWhite:0.5 alpha:1.0],
+  };
+  NSSize textSize = [label sizeWithAttributes:attrs];
+  CGFloat canvasW = ceil(textSize.width + 4.0);
+  CGFloat canvasH = ceil(kLabelHeight);
+  NSInteger pixelW = (NSInteger)(canvasW * kScale);
+  NSInteger pixelH = (NSInteger)(canvasH * kScale);
+
+  CGColorSpaceRef cs = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+  CGContextRef ctx = CGBitmapContextCreate(
+      NULL, pixelW, pixelH, 8, pixelW * 4, cs,
+      (CGBitmapInfo)kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+  CGColorSpaceRelease(cs);
+  if (!ctx)
+    return nil;
+
+  CGContextScaleCTM(ctx, kScale, kScale);
+
+  NSGraphicsContext *gc = [NSGraphicsContext graphicsContextWithCGContext:ctx
+                                                                  flipped:NO];
+  [NSGraphicsContext saveGraphicsState];
+  [NSGraphicsContext setCurrentContext:gc];
+
+  CGFloat originX = (canvasW - textSize.width) / 2.0;
+  CGFloat originY = (canvasH - textSize.height) / 2.0;
+  [label drawAtPoint:NSMakePoint(originX, originY) withAttributes:attrs];
+
+  [NSGraphicsContext restoreGraphicsState];
+
+  MTLTextureDescriptor *desc = [MTLTextureDescriptor
+      texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm
+                                   width:pixelW
+                                  height:pixelH
+                               mipmapped:NO];
+  desc.usage = MTLTextureUsageShaderRead;
+  id<MTLTexture> texture = [device newTextureWithDescriptor:desc];
+  [texture replaceRegion:MTLRegionMake2D(0, 0, pixelW, pixelH)
+             mipmapLevel:0
+               withBytes:CGBitmapContextGetData(ctx)
+             bytesPerRow:pixelW * 4];
+
+  CGContextRelease(ctx);
+  _labelTextures[idx] = texture;
+  _cachedLabels[idx] = [label copy];
+  return texture;
+}
+
 static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
                              id<MTLTexture> texture, CGPoint metalCenter,
                              float halfW, float halfH,
@@ -236,7 +309,7 @@ static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
   CGFloat totalButtonsW =
       itemCount * kButtonSize + (itemCount - 1) * kButtonSpacing;
   CGFloat toolbarW = totalButtonsW + kToolbarPadding * 2;
-  CGFloat toolbarH = kButtonSize + kToolbarPadding * 2;
+  CGFloat toolbarH = kButtonHeight + kToolbarPadding * 2;
   CGFloat toolbarX = ioW / 2.0;
   CGFloat toolbarY = ioH - kToolbarMargin - toolbarH / 2.0;
 
@@ -249,8 +322,8 @@ static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
     _buttonCenters[i] = [NSValue valueWithPoint:NSMakePoint(bx, toolbarY)];
     _buttonRects[i] =
         [NSValue valueWithRect:NSMakeRect(bx - kButtonSize / 2.0,
-                                          toolbarY - toolbarH / 2.0,
-                                          kButtonSize, toolbarH)];
+                                          toolbarY - kButtonHeight / 2.0,
+                                          kButtonSize, kButtonHeight)];
   }
 
   id<MTLCommandQueue> queue = [cache commandQueueWithRegistryID:registryID
@@ -290,7 +363,7 @@ static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
                                                     blue:0.08
                                                    alpha:0.92]);
     _highlightTexture =
-        renderRoundedRect(device, kButtonSize, kButtonSize, kHighlightCorner,
+        renderRoundedRect(device, kButtonSize, kButtonHeight, kHighlightCorner,
                           [NSColor colorWithRed:0.28
                                           green:0.28
                                            blue:0.28
@@ -310,12 +383,12 @@ static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
       CGPoint center = _buttonCenters[i].pointValue;
       CGPoint hlMetal = {center.x - ioW / 2.0f, ioH / 2.0f - center.y};
       drawTexturedQuad(encoder, _highlightTexture, hlMetal, kButtonSize / 2.0f,
-                       kButtonSize / 2.0f, viewportSize);
+                       kButtonHeight / 2.0f, viewportSize);
       break;
     }
   }
 
-  // Icons
+  // Icons (shifted up to make room for labels)
   for (NSUInteger i = 0; i < (NSUInteger)itemCount; i++) {
     id<MTLTexture> iconTex = [self textureForIcon:_items[i].iconName
                                            device:device
@@ -324,9 +397,29 @@ static void drawTexturedQuad(id<MTLRenderCommandEncoder> encoder,
       continue;
 
     CGPoint center = _buttonCenters[i].pointValue;
-    CGPoint metalPos = {center.x - ioW / 2.0f, ioH / 2.0f - center.y};
+    CGFloat iconY = center.y + kIconShiftY;
+    CGPoint metalPos = {center.x - ioW / 2.0f, ioH / 2.0f - iconY};
     drawTexturedQuad(encoder, iconTex, metalPos, kButtonSize / 2.0f,
                      kButtonSize / 2.0f, viewportSize);
+  }
+
+  // Shortcut labels inside buttons, below icons
+  for (NSUInteger i = 0; i < (NSUInteger)itemCount; i++) {
+    NSString *label = _items[i].shortcutLabel;
+    if (!label.length)
+      continue;
+    id<MTLTexture> labelTex = [self textureForLabel:label
+                                             device:device
+                                              index:i];
+    if (!labelTex)
+      continue;
+
+    CGPoint center = _buttonCenters[i].pointValue;
+    CGFloat labelY = center.y - kButtonHeight / 2.0 + kLabelHeight / 2.0 + 4.0;
+    CGPoint metalPos = {center.x - ioW / 2.0f, ioH / 2.0f - labelY};
+    float halfW = labelTex.width / (2.0f * kScale);
+    float halfH = labelTex.height / (2.0f * kScale);
+    drawTexturedQuad(encoder, labelTex, metalPos, halfW, halfH, viewportSize);
   }
 
   [encoder endEncoding];
