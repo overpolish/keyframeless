13ed5cc26bfde598bece31623a133923ce51922e
Dom <dom@overpolish.co>
2026-07-26 18:28:38 +0100

    chore: remove archived plugins

--- Archive/MagicMove/MagicMove/Plugin/Core/main.m
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2026 overpolish
- * SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
- */
-
-#import <FxPlug/FxPlugSDK.h>
-#import <KeyframelessKit/KeyframelessKit.h>
-
-KK_PLUGIN_MAIN()
--- Archive/MagicMove/kk-ai-helper/main.swift
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2026 overpolish
- * SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
- */
-
-import KeyframelessAI
-
-LocalAIHelperServer.run()
