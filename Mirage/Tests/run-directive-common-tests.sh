4628b49c5250652be120454c6fdeab62854cd71d
Dom <dom@overpolish.co>
2026-07-31 21:23:39 +0100

    refactor(Mirage): re-organize color panel files

--- Mirage/Tests/run-directive-common-tests.sh
+++ Mirage/Tests/run-directive-common-tests.sh
@@ -5,8 +5,9 @@ set -eu
 HERE="$(cd "$(dirname "$0")" && pwd)"
 SOURCE="$HERE/MirageDirectiveCommonTests.m"
 RENDER_HEADERS="$HERE/../Mirage/Plugin/Render"
+SURFACE_HEADERS="$RENDER_HEADERS/Surface"
 OUTPUT="${TMPDIR:-/tmp}/mirage-directive-common-tests"
 
 xcrun clang -fobjc-arc -framework Foundation \
-  -I"$RENDER_HEADERS" "$SOURCE" -o "$OUTPUT"
+  -I"$RENDER_HEADERS" -I"$SURFACE_HEADERS" "$SOURCE" -o "$OUTPUT"
 "$OUTPUT"
